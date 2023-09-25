67dab981bdf185b29eb074dce54b1c962634d201
yogaaaaaaaaa <yogaridwan231@gmail.com>
2023-09-25 22:32:07 +0700

    first commit

---
+++ database/migrations/2023_09_25_150528_create_pinjam_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pinjam', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_us')->index();
+            $table->integer('id_bu')->index();
+            $table->string('name')->index();
+            $table->string('buku')->index();
+            $table->string('mobile')->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pinjam');
+    }
+};
